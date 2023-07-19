37c6c275470a5c920e0f0d317def475265b469e9
KauanMP <kauanmoreira1232@gmail.com>
2023-07-19 09:25:59 -0300

    Criando o contexto, o provider e exportando o conteudo para a pagina

--- contextapi/src/pages/Home/index.jsx
+++ contextapi/src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import "./index.css";
+import { useContext } from "react";
+import { CounterContext } from "../../context/CounterContext";
 
 const Home = () => {
+  const { counter } = useContext(CounterContext);
+
   return (
     <div>
-        <h2>Page destined to Home</h2>
+      <h2>Page destined to Home</h2>
+      <p>Valor do contador {counter}</p>
     </div>
   );
-}
+};
 
 export default Home;
