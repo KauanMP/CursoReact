8cdbd45ef6c2a7cd16271ce7bdfd964b197b6c18
KauanMP <kauanmoreira1232@gmail.com>
2023-07-19 09:00:42 -0300

    Criando o projeto, instalando dependencias e criando a ' context '

---
+++ contextapi/src/pages/About/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const About = () => {
+    return (
+        <div>
+            <h2>Page destined to About</h2>
+        </div>
+    );
+}
+
+export default About;
---
+++ contextapi/src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+const Home = () => {
+  return (
+    <div>
+        <h2>Page destined to Home</h2>
+    </div>
+  );
+}
+
+export default Home;
