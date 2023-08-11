2c205187685bc493ea671acda4314777592466c3
KauanMP <kauanmoreira1232@gmail.com>
2023-08-10 22:35:12 -0300

    revendo o useEffect

---
+++ hooks-states/src/components/HookUseEffect.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+const HookUseEffect = () => {
+  // 1 - useEffect, sem dependências
+  useEffect(() => {
+    console.log("Estou sendo executado!");
+  });
+
+  const [number, setNumber] = useState(1);
+
+  const changeSomething = () => {
+    setNumber(number + 1);
+  };
+
+  // 2 - array de dependências vazio
+  useEffect(() => {
+    console.log("Serei executado apenas uma vez!");
+  }, []);
+
+  // 3 - itens no array de dependências
+  const [anotherNumber, setAnotherNumber] = useState(0);
+
+  useEffect(() => {
+    if (anotherNumber > 0) {
+      console.log("Sou executado apenas quando o anotherNumber muda!");
+    }
+  }, [anotherNumber]);
+
+  // 4 - cleanup do useEffect
+  useEffect(() => {
+    // const timer = setTimeout(() => {
+    //   console.log("hello world");
+
+    //   setAnotherNumber(anotherNumber + 1);
+    // }, 2000);
+
+    // return () => clearTimeout(timer);
+  }, [anotherNumber]);
+  return (
+    <div>
+      <h2>useEffect</h2>
+      <p>Number: {number}</p>
+      <button onClick={changeSomething}>Executar!</button>
+      <p>anotherNumber: {anotherNumber}</p>
+      <button onClick={() => setAnotherNumber(anotherNumber + 1)}>
+        Mudar anotherNumber!
+      </button>
+      <hr />
+    </div>
+  );
+};
+
+export default HookUseEffect;
