81d3de10ec41869dbaf5a6dd063842b50d2cfd74
KauanMP <kauanmoreira1232@gmail.com>
2023-08-11 15:39:46 -0300

    Revendo o useContext

--- hooks-states/src/App.jsx
+++ hooks-states/src/App.jsx
@@ -2,25 +2,28 @@ import "./App.css";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { HookUseContext } from "./components/HookUseContext";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <h1>React Hooks</h1>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">Sobre</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <HookUseContext>
+        <BrowserRouter>
+          <h1>React Hooks</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">Sobre</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </HookUseContext>
     </div>
   );
 }
