1da7305a3e2ceeb360901e34e424049579f66469
KauanMP <kauanmoreira1232@gmail.com>
2023-08-15 09:51:52 -0300

    Estudando mais sobre use Ref e aprendendo como funciona o useImperativeHandle junto do forwardRef

---
+++ hooks-states/src/components/SomeComponent.jsx
@@ -0,0 +1,25 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+// eslint-disable-next-line react/display-name
+const SomeComponent = forwardRef((props, ref) => {
+    const localInputRef = useRef();
+  
+    useImperativeHandle(ref, () => {
+      return {
+        validate: () => {
+          if (localInputRef.current.value.length >= 3) {
+            localInputRef.current.value = "";
+          }
+        },
+      };
+    });
+
+    return (
+      <div>
+        <p>Insira no minimo 3 caracteres</p>
+        <input type="text" ref={localInputRef} />
+      </div>
+    );
+  });
+
+export default SomeComponent;
