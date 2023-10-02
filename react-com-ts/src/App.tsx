99701731cd3f945dda6548a5a50140715ca80b87
KauanMP <kauanmoreira1232@gmail.com>
2023-10-02 17:23:14 -0300

    funções em componentes

--- react-com-ts/src/App.tsx
+++ react-com-ts/src/App.tsx
@@ -4,12 +4,18 @@ function App() {
   const age: number = 18;
   const isWorking: boolean = true;
 
+  // 2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  };
+
   return (
     <div>
       <h1>React com TypeScript</h1>
       <h2>Nome: {name}</h2>
       <p>Idade: {age}</p>
       {isWorking && <p>Está trabalhando!</p>}
+      <h3>{userGreeting(name)}</h3>
     </div>
   );
 }
