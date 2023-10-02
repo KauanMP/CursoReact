0103d525d3bf55acb317fe594188ef79778b2672
KauanMP <kauanmoreira1232@gmail.com>
2023-10-02 17:18:19 -0300

    Variaveis com o ts

--- react-com-ts/src/App.tsx
+++ react-com-ts/src/App.tsx
@@ -1,8 +1,15 @@
-
 function App() {
+  // 1 - Variaveis
+  const name: string = "kauan";
+  const age: number = 18;
+  const isWorking: boolean = true;
+
   return (
     <div>
       <h1>React com TypeScript</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && <p>Está trabalhando!</p>}
     </div>
   );
 }
