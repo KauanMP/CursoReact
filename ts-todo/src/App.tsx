618e63395c2990f43cefe716018093fd25124923
KauanMP <kauanmoreira1232@gmail.com>
2023-10-03 15:03:49 -0300

    Criando, estilizando e iniciando ações do formulário

--- ts-todo/src/App.tsx
+++ ts-todo/src/App.tsx
@@ -1,13 +1,28 @@
+// Components
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import TaskForm from "./components/Task/TaskForm";
+import TaskList from "./components/Task/taskList";
+
+// Css
 import styles from "./App.module.css";
-import Footer from "./components/interfaces/Footer/Footer";
-import Header from "./components/interfaces/Header/Header";
+
+// Interface
+import { ITask } from "./interfaces/Task";
 
 function App() {
   return (
     <div>
       <Header />
       <main className={styles.main}>
-        <h1>Conteúdo...</h1>
+        <div>
+          <h2>O que você vai fazer ?</h2>
+          <TaskForm btnText="Criar tarefa" />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
       </main>
       <Footer />
     </div>
