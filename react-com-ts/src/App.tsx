5f24a27fa5783d051ebbb8486422f28c572213bb
KauanMP <kauanmoreira1232@gmail.com>
2023-10-03 01:06:01 -0300

    Context API

--- react-com-ts/src/App.tsx
+++ react-com-ts/src/App.tsx
@@ -7,10 +7,20 @@ import Destructuring, { Category } from "./components/Destructuring";
 
 // 6 - useState
 import State from "./components/State";
+import { createContext } from "react";
 
 // 8 - type
 type textOrNull = string | null;
 type fixed = "Isso" | "Ou" | "Aquilo";
+
+// 9 - Context
+interface IAppContext {
+  language: string;
+  framework: string;
+  projects: number;
+}
+
+const AppContext = createContext<IAppContext | null>(null);
 
 function App() {
   // 1 - Variaveis
@@ -31,33 +41,42 @@ function App() {
 
   const testandoFixed: fixed = "Isso";
 
+  // 9 - Context
+  const contextValue: IAppContext = {
+    language: "JavaScript",
+    framework: "Express",
+    projects: 5,
+  };
+
   return (
-    <div>
-      <h1>React com TypeScript</h1>
-      <h2>Nome: {name}</h2>
-      <p>Idade: {age}</p>
-      {isWorking && <p>Está trabalhando!</p>}
-      <h3>{userGreeting(name)}</h3>
-      <FirstComponent />
-      <SecondComponent name="João" />
-      <Destructuring
-        title="Primeiro Post"
-        content="Algum conteudo"
-        commentsQty={6}
-        tags={["ts", "js"]}
-        category={Category.TS}
-      />
-      <Destructuring
-        title="Segundo Post"
-        content="Algum conteudo"
-        commentsQty={2}
-        tags={["ts", "js", "c#", "mongoDb"]}
-        category={Category.JS}
-      />
-      <State />
-      {myText && <p>Tem texto na variável</p>}
-      {mySecondtext && <p>Tem texto na variável</p>}
-    </div>
+    <AppContext.Provider value={contextValue}>
+      <div>
+        <h1>React com TypeScript</h1>
+        <h2>Nome: {name}</h2>
+        <p>Idade: {age}</p>
+        {isWorking && <p>Está trabalhando!</p>}
+        <h3>{userGreeting(name)}</h3>
+        <FirstComponent />
+        <SecondComponent name="João" />
+        <Destructuring
+          title="Primeiro Post"
+          content="Algum conteudo"
+          commentsQty={6}
+          tags={["ts", "js"]}
+          category={Category.TS}
+        />
+        <Destructuring
+          title="Segundo Post"
+          content="Algum conteudo"
+          commentsQty={2}
+          tags={["ts", "js", "c#", "mongoDb"]}
+          category={Category.JS}
+        />
+        <State />
+        {myText && <p>Tem texto na variável</p>}
+        {mySecondtext && <p>Tem texto na variável</p>}
+      </div>
+    </AppContext.Provider>
   );
 }
 
